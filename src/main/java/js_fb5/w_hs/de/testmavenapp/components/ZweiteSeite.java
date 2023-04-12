8c8b3024de62fb9161553350eff7b3386d720ad7
Jens Schulze <jens@Mac-mini-2018-egpu.fritz.box>
2023-04-12 18:29:55 +0200

    java localdate  Committer: Jens Schulze <jens@Mac-mini-2018-egpu.fritz.box>

--- src/main/java/js_fb5/w_hs/de/testmavenapp/components/ZweiteSeite.java
+++ src/main/java/js_fb5/w_hs/de/testmavenapp/components/ZweiteSeite.java
@@ -1,5 +1,8 @@
 package js_fb5.w_hs.de.testmavenapp.components;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import com.webobjects.appserver.WOActionResults;
 import com.webobjects.appserver.WOContext;
 import com.webobjects.eocontrol.EOEditingContext;
@@ -7,6 +10,7 @@ import com.webobjects.foundation.NSArray;
 
 import er.extensions.components.ERXComponent;
 import er.extensions.eof.ERXEC;
+import er.extensions.formatters.ERXLocalDateFormatter;
 import js_fb5.w_hs.model.MeinUser;
 
 public class ZweiteSeite extends ERXComponent {
@@ -14,6 +18,10 @@ public class ZweiteSeite extends ERXComponent {
 	private static final long serialVersionUID = 1L;
 	private EOEditingContext _editingContext;
 	private NSArray<MeinUser> uListe;
+	DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+	public ERXLocalDateFormatter formatter = new ERXLocalDateFormatter(formatter1, "dd-MM-yyyy");
+
+	  
     public ZweiteSeite(WOContext context) {
         super(context);
         _editingContext = ERXEC.newEditingContext(); 
@@ -24,8 +32,21 @@ public class ZweiteSeite extends ERXComponent {
     }
     public MeinUser aUser, aU;
     public WOActionResults addUser() {
-    	aUser = MeinUser.createMeinUser(_editingContext, "test");
+    	aUser = MeinUser.createMeinUser(_editingContext);
+    	aUser.setVorName("test");
     	return null;
+    }
+	private LocalDate ferienstartDatum;
+    public LocalDate getFerienstartDatum() {
+    	if (aU!=null) {
+    		ferienstartDatum = aU.myTimeInJava();
+    	}
+    	return ferienstartDatum;
+    }
+    public void setFerienstartDatum(LocalDate d) {
+    	if (aU!=null) {
+    		ferienstartDatum = d;
+    	}
     }
     public WOActionResults deleteUser() {
     	try {
